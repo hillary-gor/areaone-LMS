9ba43c8f8d725d7f975711d25301ab3f1b4daa61
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2024-11-14 23:59:10 +0300

    Updated isAuthenticated middleware:

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,21 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { CatchAsyncError } from "./catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { decode } from "punycode";
 
-export const isAuthenticated = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+export const isAuthenticated = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     const access_token = req.cookies.access_token;
 
     if (!access_token) {
-        return next(new ErrorHandler("Please login to access this route", 401));
+      return next(new ErrorHandler("Please login to access this route", 400));
     }
 
-    const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string) as JwtPayload;
+    const decoded = jwt.verify(
+      access_token,
+      process.env.ACCESS_TOKEN as string
+    ) as JwtPayload;
 
     if (!decoded) {
-        return next(new ErrorHandler("Invalid access token", 401));
+      return next(new ErrorHandler("Invalid access token", 400));
     }
 
-    
-})
+    req.user = JSON.parse(user);
+
+    next();
+  }
+);
